5cea84ad49e4ac35ea084356c2898808bfc591e8
Nikos Giakoumoglou <ngiakoumoglou@hotmail.com>
2022-09-07 21:27:22 +0300

    Update augmentations.py

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -28,8 +28,8 @@ class Albumentations:
                             #A.CLAHE(p=0.5), 
                             #A.ColorJitter(p=0.5),
                             #A.HueSaturationValue(p=0.5),
-                            A.RandomBrightness(p=0.2),
-                            #A.RandomContrast(p=0.2),
+                            #A.RandomBrightness(p=0.2),
+                            A.RandomContrast(p=0.2),
                             ],
                             bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
 
